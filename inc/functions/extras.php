83c231edf1aad74c2272113cf6499225817b6ebd
Dan Northern <git@dannorthern.com>
2026-06-06 00:34:35 -0400

    fix: consolidate featured image list-table admin styles
    Output one admin_head style block for all post types and hide the
    featured image column on mobile so responsive list tables expand correctly.

--- inc/functions/extras.php
+++ inc/functions/extras.php
@@ -15,6 +15,46 @@
  */
 
 /**
+ * @return array<string, int>
+ */
+if (!function_exists('extended_cpts_extras_featured_image_styles')) {
+	function &extended_cpts_extras_featured_image_styles()
+	{
+		static $styles = [];
+
+		return $styles;
+	}
+
+	function extended_cpts_extras_print_featured_image_column_styles(): void
+	{
+		$styles = extended_cpts_extras_featured_image_styles();
+
+		if (empty($styles)) {
+			return;
+		}
+
+		echo "<style>\n";
+
+		foreach ($styles as $post_type => $width) {
+			$post_type = esc_attr($post_type);
+			echo ".post-type-{$post_type} .column-featured_image { width: {$width}px; }\n";
+			echo ".post-type-{$post_type} .column-featured_image img { aspect-ratio: 4 / 3; object-fit: cover; }\n";
+		}
+
+		echo "@media screen and (max-width: 782px) {
+			.wp-list-table th.column-featured_image,
+			.wp-list-table td.column-featured_image {
+				display: none !important;
+				visibility: hidden !important;
+				height: 0 !important;
+				padding: 0 !important;
+			}
+		}
+		</style>";
+	}
+}
+
+/**
  * Add additional features to existing post types
  *
  * @param array $post_types
@@ -25,18 +65,22 @@ if (!function_exists('extended_post_type_extras')) {
 	{
 		$post_types = (array) $post_types;
 
-		foreach ($post_types as $post_type) {
-			// Handle featured image width
-			if (!empty($options['featured_image_column_width'])) {
-				add_action('admin_head', function () use ($post_type, $options) {
-					$width = $options['featured_image_column_width'];
-					echo "<style>
-						.post-type-{$post_type} .column-featured_image { width: {$width}px; }
-						.post-type-{$post_type} .column-featured_image img {  aspect-ratio: 4 / 3; object-fit: cover; }
-					</style>";
-				});
+		if (!empty($options['featured_image_column_width'])) {
+			$styles = &extended_cpts_extras_featured_image_styles();
+
+			foreach ($post_types as $post_type) {
+				$styles[$post_type] = (int) $options['featured_image_column_width'];
 			}
 
+			if (
+				function_exists('add_action')
+				&& !has_action('admin_head', 'extended_cpts_extras_print_featured_image_column_styles')
+			) {
+				add_action('admin_head', 'extended_cpts_extras_print_featured_image_column_styles');
+			}
+		}
+
+		foreach ($post_types as $post_type) {
 			// Remove meta boxes
 			if (!empty($options['remove_meta_boxes'])) {
 				add_action('add_meta_boxes', function () use ($post_type, $options) {
